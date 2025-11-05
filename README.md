5efcdfaa869ccb876270ae9928962370505a5b9f
Matthias Ferber <matthias.ferber@gmail.com>
2025-11-05 17:39:11 -0500

    Initial version
    - uv init project
    - add Litestar dependency
    - add ruff dev dependency
    - "hello world"

---
+++ README.md
@@ -0,0 +1,1 @@
+Back-end API for TV chart tracker.
---
+++ app.py
@@ -0,0 +1,7 @@
+from litestar import Litestar, get
+
+@get("/")
+async def hello_world() -> str:
+    return "hello world"
+
+app = Litestar(route_handlers=[hello_world])
