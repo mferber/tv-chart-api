998222ed0f5abb2b8e23edc207355019bfece890
Matthias Ferber <matthias.ferber@gmail.com>
2026-01-18 10:12:48 -0500

    Switched auth mechanism to JWT cookie

--- src/setup/litestar_users/plugin.py
+++ src/setup/litestar_users/plugin.py
@@ -1,4 +1,6 @@
-from litestar.security.jwt import JWTAuth
+import datetime
+
+from litestar.security.jwt import JWTCookieAuth
 from litestar_users import LitestarUsersConfig, LitestarUsersPlugin
 from litestar_users.config import (
     AuthHandlerConfig,
@@ -23,11 +25,13 @@ def configure_litestar_users_plugin(jwt_encoding_secret: str) -> LitestarUsersPl
     return LitestarUsersPlugin(
         config=LitestarUsersConfig(
             # which type of authentication to use
-            auth_backend_class=JWTAuth,
+            auth_backend_class=JWTCookieAuth,
             # True to require email verification after user registers
             require_verification_on_registration=False,  # for now assume emails are fine
             # JWT signing secret
             secret=jwt_encoding_secret,
+            # Cookie expiration
+            default_token_expiration=datetime.timedelta(days=30),
             # SQLAlchemy model for user records
             user_model=User,
             # DTO for registering users to db
