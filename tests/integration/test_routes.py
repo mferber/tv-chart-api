9092c27115cbbce83c8bb2f19222593cd4c9fcf2
Matthias Ferber <matthias.ferber@gmail.com>
2026-02-16 17:35:42 -0500

    Added raise_for_status to route tests

--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -5,6 +5,7 @@ from litestar.testing import TestClient
 @pytest.mark.parametrize("login_as_user", ["test_user1"], indirect=True)
 def test_user1_sees_own_shows(test_client: TestClient, login_as_user: None) -> None:
     rsp = test_client.get("/shows")
+    rsp.raise_for_status()
     rsp_contents = rsp.json()
     assert len(rsp_contents) == 1
     assert rsp_contents[0]["title"] == "All Creatures Great & Small"
@@ -15,6 +16,7 @@ def test_user2_sees_own_shows(
     test_client: TestClient, csrf_token_header: dict[str, str], login_as_user: None
 ) -> None:
     rsp = test_client.get("/shows")
+    rsp.raise_for_status()
     rsp_contents = rsp.json()
     assert len(rsp_contents) == 1
     assert rsp_contents[0]["title"] == "Pluribus"
