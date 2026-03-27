f863b06f473a1e4bc0fc80b92b40efdff2a09c35
Matthias Ferber <matthias.ferber@gmail.com>
2026-03-27 16:47:13 -0400

    Marked private methods in export service

--- src/services/export.py
+++ src/services/export.py
@@ -11,21 +11,21 @@ class ExportService:
 
     async def export(self) -> str:
         shows = await self.show_service.get_shows()
-        exportable = [self.exportable_show(show) for show in shows.values()]
+        exportable = [self.__exportable_show(show) for show in shows.values()]
         return json.dumps(exportable)
 
-    def exportable_show(self, show: Show) -> dict[str, Any]:
+    def __exportable_show(self, show: Show) -> dict[str, Any]:
         show_dict = show.__dict__.copy()
         del show_dict["id"]
         show_dict["image_lg_url"] = str(show_dict["image_lg_url"])
         show_dict["image_sm_url"] = str(show_dict["image_sm_url"])
         show_dict["seasons"] = [
-            [self.exportable_episode(ep) for ep in season]
+            [self.__exportable_episode(ep) for ep in season]
             for season in show_dict["seasons"]
         ]
         return show_dict
 
-    def exportable_episode(self, episode: EpisodeDescriptor) -> dict[str, Any]:
+    def __exportable_episode(self, episode: EpisodeDescriptor) -> dict[str, Any]:
         return {
             "title": episode.title,
             "watched": episode.watched,
