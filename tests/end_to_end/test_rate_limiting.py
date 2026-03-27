f7c7303d3fee82f4402655ef2cc318960449f6c0
Matthias Ferber <matthias.ferber@gmail.com>
2026-03-27 14:06:59 -0400

    Removed stray print in test

--- tests/end_to_end/test_rate_limiting.py
+++ tests/end_to_end/test_rate_limiting.py
@@ -7,7 +7,6 @@ from create_app import RATE_LIMIT_REQ_PER_MIN
 def test_rate_limiting(test_client: TestClient) -> None:
     for i in range(0, RATE_LIMIT_REQ_PER_MIN):
         resp = test_client.get("/health")
-        print(i + 1, resp.status_code)
         assert resp.status_code == httpx.codes.OK
 
     fail_resp = test_client.get("/health")
